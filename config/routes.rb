b9bf0e419f263929bb9cf79a788f868471141c4c
Hunter Hewitt <hunterchewitt@hotmail.com>
2021-01-21 22:46:27 -0600

    minute

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/stocks/:id',to:'company#show'
+  get '/stocks_minute/:id',to:'company#show_minute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
